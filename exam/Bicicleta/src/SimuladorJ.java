89dde1a96fc0bc15925e3b71876f357086626160
SantiagoR07 <141367246+SantiagoR07@users.noreply.github.com>
2023-11-16 15:31:55 -0500

    Add files via upload

--- exam/Bicicleta/src/SimuladorJ.java
+++ exam/Bicicleta/src/SimuladorJ.java
@@ -15,32 +15,54 @@ import javax.swing.Timer;
 public class SimuladorJ extends JFrame{
     
     private JButton PedalIz,PedalDer;
-    private JLabel Indicador1,Indicador2;
+    private JLabel Indicador1,Indicador2, TemporizadorLabel;
+    private Timer timer;
+    private int contadorPedalIz, contadorPedalDer, sT, velocidad;
     
    public SimuladorJ(){
        setTitle("Simulador de bicicleta estatica");
-       setSize(400,200);
+       setSize(800,400);
        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       
+       TemporizadorLabel = new JLabel("Temporizador");
+       Indicador1 = new JLabel("Pedalazos");
+       Indicador2 = new JLabel("Velocidad");
        
        PedalIz = new JButton("Pedal izquierdo");
        PedalDer = new JButton ("Pedal derecho");
        
-       Indicador1 = new JLabel("indicador 1:0");
-       Indicador2 = new JLabel("indicador 2:0");
-       
-       
        setLayout(new GridLayout(2,2));
-       add (PedalIz);
-       add(PedalDer);
+       add(TemporizadorLabel);
        add(Indicador1);
        add(Indicador2);
+       add (PedalIz);
+       add(PedalDer);
+       
+       timer = new Timer(1000,new ActionListener(){
+           @Override
+           public void actionPerformed(ActionEvent e){
+           Indicador1.setText("Pedales dados: "+contadorPedalIz);
+           Indicador2.setText("Velocidad: "+ "pedales/minuto");
+           TemporizadorLabel.setText(timer);
+           }
+       });
+       timer.start();
        
        PedalIz.addActionListener(new ActionListener(){
        @Override
        public void actionPerformed(ActionEvent e){
            PedalDer.setVisible(true);
            PedalIz.setVisible(false);
+           contadorPedalIz++;
        }});
+       PedalDer.addActionListener(new ActionListener(){
+       @Override
+       public void actionPerformed(ActionEvent e){
+           PedalDer.setVisible(false);
+           PedalIz.setVisible(true);
+           
+       }});
+       
    }
        public static void main(String[] args){
            SwingUtilities.invokeLater(new Runnable(){
