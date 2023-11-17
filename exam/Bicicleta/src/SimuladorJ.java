9005dd697a9da1fb9f823c4b680488e08a0e811a
SantiagoR07 <141367246+SantiagoR07@users.noreply.github.com>
2023-11-16 20:34:03 -0500

    Add files via upload

--- exam/Bicicleta/src/SimuladorJ.java
+++ exam/Bicicleta/src/SimuladorJ.java
@@ -12,88 +12,97 @@ import javax.swing.Timer;
  * @author Santiago Rubiano
  */
 
-public class SimuladorJ extends JFrame{
-    
-    private JButton PedalIz,PedalDer;
-    private JLabel Indicador1,Indicador2, TemporizadorLabel;
+public class SimuladorJ extends JFrame {
+
+    private JButton PedalIz, PedalDer;
+    private JLabel Indicador1, Indicador2, TemporizadorLabel;
     private Timer timer;
-    private int contadorPedalIz, contadorPedalDer, sT, PedalesSol;
+    private int contadorPedalIz, contadorPedalDer, sT;
     private double velocidad;
-   public SimuladorJ(){
-       //tamaño y distrubicion del frame
-       setTitle("Simulador de bicicleta estatica");
-       setSize(800,400);
-       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       
-       
-       //cositas del frame, botones, labels...
-       TemporizadorLabel = new JLabel("Temporizador");
-       Indicador1 = new JLabel("Pedales dados: ");
-       Indicador2 = new JLabel("Velocidad ");
-       
-       PedalIz = new JButton("Pedal izquierdo");
-       PedalDer = new JButton ("Pedal derecho");
-       
+    private int limitePedalIzquierdo = 0;
+    private int limitePedalDerecho = 0;
+
+    public SimuladorJ() {
+        setTitle("Simulador de bicicleta estatica");
+        setSize(800, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        TemporizadorLabel = new JLabel("Temporizador");
+        Indicador1 = new JLabel("Pedales dados: ");
+        Indicador2 = new JLabel("Velocidad ");
+
         JTextField PedalesSolField = new JTextField(2);
-       
-       JPanel panelClicks = new JPanel();
-       panelClicks.add(new JLabel("Limite de clicks: "));
-       panelClicks.add(PedalesSolField);
-       
-       
-       //añadimos las cositas en el orden que le damos abajo en este caso  2x2
-       setLayout(new GridLayout(3,3));
-       add (panelClicks);
-       add(TemporizadorLabel);
-       add(Indicador1);
-       add(Indicador2);
-       add (PedalIz);
-       add(PedalDer);
-       
-       //temporizador e indicadores de solicitud
-       
-       timer = new Timer(1000,new ActionListener(){
-           @Override
-           public void actionPerformed(ActionEvent e){
-           sT++;
-           velocidad = contadorPedalIz/sT;                  // La velocidad no imprime datos decimales
-           TemporizadorLabel.setText("Tiempo transcurrido: "+ sT + " segundos");
-           Indicador1.setText("Pedales dados: "+contadorPedalIz);
-           Indicador2.setText("Velocidad: "+ velocidad + " pedales/minuto");
-           
-          // get.Text.panelClicks
-           
-           }
-       });
-       timer.start();
-       
-       
-       //acciones de los pedales
-       PedalIz.addActionListener(new ActionListener(){
-       @Override
-       public void actionPerformed(ActionEvent e){
-           PedalDer.setVisible(true);
-           PedalIz.setVisible(false);
-           contadorPedalIz++;
-       }});
-       PedalDer.addActionListener(new ActionListener(){
-       @Override
-       public void actionPerformed(ActionEvent e){
-           PedalDer.setVisible(false);
-           PedalIz.setVisible(true);
-           
-       }});
-       
-   }
-   //main
-   
-       public static void main(String[] args){
-           SwingUtilities.invokeLater(new Runnable(){
-               @Override
-               public void run(){
-                   new SimuladorJ().setVisible(true);
-               }
-           });
-       
-   }
+
+        JPanel panelClicks = new JPanel();
+        panelClicks.add(new JLabel("Limite de clicks(se debe dar enter para que lo aplique): "));
+        panelClicks.add(PedalesSolField);
+        add(panelClicks);
+
+        setLayout(new GridLayout(3, 3));
+        add(TemporizadorLabel);
+        add(Indicador1);
+        add(Indicador2);
+
+        PedalIz = new JButton("Pedal izquierdo");
+        PedalDer = new JButton("Pedal derecho");
+
+        add(PedalIz);
+        add(PedalDer);
+
+        timer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sT++;
+                velocidad = contadorPedalIz / sT;
+                TemporizadorLabel.setText("Tiempo transcurrido: " + sT + " segundos");
+                Indicador1.setText("Pedales dados: " + contadorPedalIz);
+                Indicador2.setText("Velocidad: " + velocidad + " pedales/minuto");
+            }
+        });
+        timer.start();
+
+        PedalIz.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                contadorPedalIz++;
+                if (contadorPedalIz >= limitePedalIzquierdo) {
+                    PedalDer.setVisible(true);
+                    PedalIz.setVisible(false);
+                }
+            }
+        });
+
+        PedalDer.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                contadorPedalDer++;
+                if (contadorPedalDer >= limitePedalDerecho) {
+                    PedalDer.setVisible(false);
+                    PedalIz.setVisible(true);
+                }
+            }
+        });
+
+        PedalesSolField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    int limiteTotal = Integer.parseInt(PedalesSolField.getText());
+                    limitePedalIzquierdo = limiteTotal;
+                    limitePedalDerecho = limiteTotal;
+                } catch (NumberFormatException ex) {
+                    // Manejar la entrada no numérica si es necesario
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new SimuladorJ().setVisible(true);
+            }
+        });
+    }
 }
